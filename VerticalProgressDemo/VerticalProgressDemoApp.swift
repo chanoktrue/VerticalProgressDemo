236326ac337d07d0812ffa31a78bae2454ad92f8
chanoktrue <chanoktrue@me.com>
2023-12-21 05:45:44 +0700

    Initial Commit

---
+++ VerticalProgressDemo/VerticalProgressDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalProgressDemoApp.swift
+//  VerticalProgressDemo
+//
+//  Created by Thongchai Subsaidee on 21/12/23.
+//
+
+import SwiftUI
+
+@main
+struct VerticalProgressDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
